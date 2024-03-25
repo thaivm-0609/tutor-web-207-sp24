13c93133450c0c6c3e0c09185407c06a25cb6813
lovent <vuongthai95@gmail.com>
2024-03-25 22:47:37 +0700

    B01-luongnv16-thaivm2

---
+++ controllers/DetailController.js
@@ -0,0 +1,6 @@
+window.DetailController = function ($scope,$http,$routeParams,$location) {
+    //$scope: quản lý biến, hàm
+    //$http: call api 
+    //$routeParams: lấy tham số được truyền qua url
+    //$location: điều hướng
+}
---
+++ controllers/ListController.js
@@ -0,0 +1,7 @@
+window.ListController = function ($scope) {
+    $scope.tenBien = ''; //khai báo biến
+
+    $scope.tenHam = function () { //khai báo hàm
+
+    }
+}
